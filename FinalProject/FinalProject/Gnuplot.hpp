6ca9f0138de33aa82f926874d33d7c04276ee90f
PeishanLiu <pl2242@nyu.edu>
2021-12-10 15:58:13 -0500

    Add Gnuplot h file

---
+++ FinalProject/FinalProject/Gnuplot.hpp
@@ -0,0 +1,22 @@
+
+
+//  Created by PeishanLiu on 2021-12-10.
+
+#ifndef Gnuplot_hpp
+#define Gnuplot_hpp
+
+#include<iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+
+
+void Gnuplot_CAAR(map<string, vector<double>> research_result, string type);
+
+
+#endif /* Gnuplot_hpp */
