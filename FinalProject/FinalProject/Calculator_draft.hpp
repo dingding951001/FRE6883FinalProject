b91e28400e89ead4ff9c83d3f34c13c81414c73d
jz <44959696+juntaoz2@users.noreply.github.com>
2021-12-11 20:09:33 -0500

    Update Calculator_draft.hpp

--- FinalProject/FinalProject/Calculator_draft.hpp
+++ FinalProject/FinalProject/Calculator_draft.hpp
@@ -14,69 +14,20 @@
 #include <cmath>
 #include <vector>
 #include <map>
-#endif /* Calculator_hpp */
+#pragma once
+
 using namespace std;
 
 typedef vector<double> Vector;
 typedef vector<Vector> Matrix;
 
-//input matrix, column是时间，row是80个stock
+Vector operator+(Matrix &m,Vector &v);
+Vector AAR(Matrix &m);
+Vector CAAR(const Vector &AAR_v);
 
-double mean(const Vector &v){
-    double summ = 0.0;
-    for_each(v.begin(), v.end(), [&] (double n) {
-        summ += n;});
-    return summ/v.size();
-}
+Vector mean(const Matrix &m2);
+Vector stdev(const Matrix &m2);
 
-double stdev(const Vector &v) {
-    double avg = mean(v);
-    double init = 0.0;
-    for (int i = 0;i<v.size();i++) {
-        init += pow(v[i]-avg, 2);
-    }
-    return sqrt(init / v.size());
-}
+Matrix call_bootstrap();
 
-Vector AAR_v(const Matrix &m) {
-    Vector v(m.size());
-    for (int i = 0;i<m.size();i++){
-        v[i] = mean(m[i]);
-    }
-    return v;
-};
-
-double AAR_mean(Vector &AAR_v) {
-    return mean(AAR_v);
-}
-
-Vector AAR_STD_v(Matrix &m){
-    Vector v(m.size());
-    for (int i = 0;i<m.size();i++){
-        v[i] = stdev(m[i]);
-    }
-    return v;
-};
-
-double AAR_STD(Vector &AAR_STD_V) {
-    return mean(AAR_STD_V);
-}
-//-------------------------------------
-//CAAR_v 是vector，[AAR1, AAR1+AAR2, AAR1+AAR2+AAR3, ...]以此类推
-Vector CAAR_v(const Vector &AAR_v){
-    double init = 0.0;
-    Vector v(AAR_v.size());
-    for (int i=0;i<AAR_v.size();i++) {
-        init += AAR_v[i];
-        v[i] = init;
-    }
-    return v;
-};
-
-double CAAR_mean(Vector &CAAR_v) {
-    return mean(CAAR_v);
-}
-
-double CAAR_STD_v(Vector &CAAR_v){
-    return stdev(CAAR_v);
-};
+#endif /* Calculator_hpp */
