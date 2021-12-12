004863ac32b0d8ae66d583fa0a3a4e89fb962ac6
jz <44959696+juntaoz2@users.noreply.github.com>
2021-12-11 20:40:41 -0500

    Update Calculator.hpp

--- FinalProject/FinalProject/Calculator.hpp
+++ FinalProject/FinalProject/Calculator.hpp
@@ -15,6 +15,7 @@
 #include <vector>
 #include <map>
 #pragma once
+#include "Bootstrap.hpp"
 
 using namespace std;
 
@@ -28,6 +29,6 @@ Vector CAAR(const Vector &AAR_v);
 Vector mean(const Matrix &m2);
 Vector stdev(const Matrix &m2);
 
-Matrix call_bootstrap();
+Matrix call_bootstrap(vector<Stock*> stocks,const StockPrice& benchmark, const int n);
 
 #endif /* Calculator_hpp */
